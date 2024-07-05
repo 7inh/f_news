8f092b47d30573228791a93745a279de9365de55
Minh Tri <nguyenminhtri1808t@gmail.com>
2024-07-05 18:07:17 +0700

    feat(chart): custom style chart weather

--- components/utility-weather/utility-weather.js
+++ components/utility-weather/utility-weather.js
@@ -1,27 +1,62 @@
-var chart = Highcharts.chart('container-chart', {
+Highcharts.chart('container-chart', {
   chart: {
-    type: 'bar',
+    type: 'areaspline',
+    zooming: {
+      type: 'xy',
+    },
   },
   title: {
-    text: 'Fruit Consumption',
+    text: 'Dự báo thời tiết',
   },
   xAxis: {
-    categories: ['Apples', 'Bananas', 'Oranges'],
+    categories: ['17:00', '20:00', '23:00', '02:00', '05:00', '09:00'],
+    crosshair: true,
+    title: {
+      text: 'Time',
+    },
   },
   yAxis: {
     title: {
-      text: 'Fruit eaten',
+      text: 'Nhiệt độ (°C)',
     },
   },
-
+  plotOptions: {
+    areaspline: {
+      fillColor: {
+        linearGradient: {
+          x1: 0,
+          y1: 0,
+          x2: 0,
+          y2: 1,
+        },
+        stops: [
+          [0, 'rgba(253, 214, 99, 0.4)'], // Màu vàng nhạt phía trên
+          [1, 'rgba(255, 214, 99, 0.5)'], // Màu vàng đậm phía dưới
+        ],
+      },
+      marker: {
+        radius: 5,
+      },
+      lineWidth: 3,
+      states: {
+        hover: {
+          lineWidth: 1,
+        },
+      },
+      threshold: null,
+    },
+  },
+  legend: {
+    enabled: false,
+  },
   series: [
     {
-      name: 'Jane',
-      data: [1, 0, 4],
-    },
-    {
-      name: 'John',
-      data: [5, 7, 3],
+      name: 'Nhiệt độ',
+      data: [0, 30, 20, 15, 5, 35],
+      tooltip: {
+        valueSuffix: ' độ C',
+      },
+      color: '#FFCC02',
     },
   ],
 });
