e8af5f49331ea71b81996ac143ccfdf2ba13127e
imdanielbui <duykhanhb60@gmail.com>
2024-07-05 21:33:06 +0700

    fixed/l11list-item-column

--- components/list-news-column/list-news-column.js
+++ components/list-news-column/list-news-column.js
@@ -3,12 +3,3 @@ fetch("./components/list-news-column/item-news.html")
         .then((data) => {
           document.querySelector("#list-news-column").innerHTML = data.repeat(8);
         });
-
-document
-        .getElementById("list-news-column")
-        .addEventListener("click", function () {
-          const htmlElement = document.documentElement;
-          const currentTheme = htmlElement.getAttribute("data-theme");
-          const newTheme = currentTheme === "light" ? "dark" : "light";
-          htmlElement.setAttribute("data-theme", newTheme);
-        });
