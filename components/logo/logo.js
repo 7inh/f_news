0579559840aaf43bc291072a71f40978de7b7bf7
dangkhoa <dangkhoa@doctogo.vn>
2024-07-08 15:31:42 +0700

    feat: logo

--- components/logo/logo.js
+++ components/logo/logo.js
@@ -18,7 +18,7 @@ async function checkWeather(city) {
     const response = await fetch(apiUrl + city + appId);
 
     if (response.status == "404") {
-        return;
+       return this.checkWeather(city);
     } else {
         var data = await response?.json();
         if (data.weather[0].main == "Clear") {
