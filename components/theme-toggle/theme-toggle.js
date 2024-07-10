c0dbb047f40eb53849ef0ffd3f45b14209e8ca7d
minhthanh <autobot.tm19@gmail.com>
2024-07-10 14:55:53 +0700

    style: replace from css to tailwind css on theme toggle section

--- components/theme-toggle/theme-toggle.js
+++ components/theme-toggle/theme-toggle.js
@@ -1,6 +1,7 @@
 function toggleTheme() {
   const theme = document.documentElement.getAttribute('data-theme');
-  const newTheme = theme === 'light' ? 'dark' : 'light';
+  console.log('toggle theme', theme);
+  const newTheme = theme === 'dark' ? 'light' : 'dark';
   document.documentElement.setAttribute('data-theme', newTheme);
   localStorage.setItem('theme', newTheme);
 }
