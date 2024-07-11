9a47c6234442d54e545f8e2c8c079f7892408f74
Minh Tri <nguyenminhtri1808t@gmail.com>
2024-07-11 14:29:36 +0700

    feat(chart): add chart wind and using highChart library for chart temperature

--- components/utility-weather/utility-weather.js
+++ components/utility-weather/utility-weather.js
@@ -113,3 +113,82 @@ Highcharts.chart('precipitation-chart', {
 });
 
 // Chart temperature
+
+Highcharts.chart('temperature-chart', {
+  chart: {
+    type: 'areaspline',
+    zooming: {
+      type: 'xy',
+    },
+  },
+  title: {
+    text: '',
+  },
+  xAxis: {
+    categories: ['17:00', '20:00', '23:00', '02:00', '05:00', '09:00'],
+    crosshair: true,
+    title: {
+      text: '',
+    },
+  },
+  yAxis: {
+    title: {
+      text: '',
+    },
+    labels: {
+      enabled: false,
+    },
+  },
+  plotOptions: {
+    areaspline: {
+      fillColor: {
+        linearGradient: {
+          x1: 0,
+          y1: 0,
+          x2: 0,
+          y2: 1,
+        },
+        stops: [
+          [0, 'rgba(253, 214, 99, 0.4)'],
+          [1, 'rgba(255, 214, 99, 0.5)'],
+        ],
+      },
+      marker: {
+        radius: 5,
+      },
+      lineWidth: 3,
+      states: {
+        hover: {
+          lineWidth: 1,
+        },
+      },
+      threshold: null,
+    },
+  },
+  legend: {
+    enabled: false,
+  },
+  tooltip: {
+    enabled: true,
+  },
+  series: [
+    {
+      name: 'Nhiệt độ',
+      data: [0, 30, 20, 15, 5, 35],
+      tooltip: {
+        valueSuffix: ' độ C',
+      },
+      color: '#FFCC02',
+      dataLabels: {
+        enabled: true,
+        format: '{y}°C',
+        style: {
+          color: 'gray',
+          fontWeight: 'bold',
+          textOutline: 'none',
+        },
+        y: -10,
+      },
+    },
+  ],
+});
