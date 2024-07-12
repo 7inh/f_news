c998e5dcf22a27a3e36ac849a8e06d1b3e1de9f5
Nguyễn Long Duy <88131939+Duy2401@users.noreply.github.com>
2024-07-12 15:40:54 +0700

    fix: API of Football

--- components/utility-football/utility-football.js
+++ components/utility-football/utility-football.js
@@ -1,21 +1,5 @@
 function GetDaTa() {
-  const apiKey = '1aa47cf8906c45df834db19a3265fac5';
-  const params = {
-    season: 2023,
-    dateFrom: '2023-05-17',
-    dateTo: '2023-05-20',
-    limit: 10, // Giới hạn trả về 10 trận đấu
-  };
-  const queryString = new URLSearchParams(params).toString();
-  fetch(
-    `https://api.football-data.org/v4/competitions/PL/matches?${queryString}`,
-    {
-      method: 'GET',
-      headers: {
-        'X-Auth-Token': apiKey,
-      },
-    }
-  )
+  fetch('https://api.openligadb.de/getmatchdata/em/2024')
     .then((response) => {
       if (!response.ok) {
         throw new Error('Network response was not ok ' + response.statusText);
@@ -23,27 +7,63 @@ function GetDaTa() {
       return response.json(); // Chuyển đổi dữ liệu nhận được sang JSON
     })
     .then((data) => {
-      console.log(data.matches.map((item) => item.awayTeam.name));
       const listMatch = document.querySelector('#utility-football-lists');
-      console.log(listMatch);
       let html = '';
-      data.matches.forEach((match) => {
-        html += `
+      data.forEach((match) => {
+        if (match.matchResults.length > 0) {
+          html += `
         <div class="utility-football-item flex relative mb-4 pb-3 items-center text-center justify-evenly">
                 <div class="football-item-info">
-                  <img class="logo w-full h-full object-cover" src="${match.awayTeam.crest}"/>
-                  <div class="football-item-name mt-3 text-xs font-normal">${match.awayTeam.shortName}</div>
+                  <img class="logo w-full h-full object-cover" src="${
+                    match.team1.teamIconUrl
+                  }"/>
+                  <div class="football-item-name mt-3 text-xs font-normal">${
+                    match.team1.shortName
+                  }</div>
                 </div>
                 <p class="football-item-score article-bold">
-                  <span>${match.score.fullTime.away}</span>
+                  <span>${
+                    match.matchResults.map((item) => item.pointsTeam1)[0]
+                  }</span>
                   <span> - </span>
-                  <span>${match.score.fullTime.home}</span>
+                  <span>${
+                    match.matchResults.map((item) => item.pointsTeam2)[0]
+                  }</span>
                 </p>
                 <div class="football-item-info">
-                  <img class="logo w-full h-full object-cover" src="${match.homeTeam.crest}"/>
-                  <div class="football-item-name mt-3 text-xs font-normal">${match.homeTeam.shortName}</div>
+                  <img class="logo w-full h-full object-cover" src="${
+                    match.team2.teamIconUrl
+                  }"/>
+                  <div class="football-item-name mt-3 text-xs font-normal">${
+                    match.team2.shortName
+                  }</div>
                 </div>
         </div>`;
+        } else {
+          const dateTime = new Date(match.matchDateTime);
+          html += `
+        <div class="utility-football-item flex relative mb-4 pb-3 items-center text-center justify-evenly">
+                <div class="football-item-info">
+                  <img class="logo w-full h-full object-cover" src="${
+                    match.team1.teamIconUrl
+                  }"/>
+                  <div class="football-item-name mt-3 text-xs font-normal">${
+                    match.team1.shortName
+                  }</div>
+                </div>
+                <p class="football-item-score text-base font-semibold">
+                  <span>${dateTime.toLocaleDateString()}</span>
+                </p>
+                <div class="football-item-info">
+                  <img class="logo w-full h-full object-cover" src="${
+                    match.team2.teamIconUrl
+                  }"/>
+                  <div class="football-item-name mt-3 text-xs font-normal">${
+                    match.team2.shortName
+                  }</div>
+                </div>
+        </div>`;
+        }
       });
       listMatch.innerHTML = html;
     })
